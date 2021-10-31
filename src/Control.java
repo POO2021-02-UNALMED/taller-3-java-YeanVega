cb68548f3548fd9ce01bc44cb852bec70836abfb
Yeferson Andres Velazco Galvis <92173102+YeanVega@users.noreply.github.com>
2021-10-31 18:17:35 -0500

    Add files via upload

---
+++ src/Control.java
@@ -0,0 +1,64 @@
+package taller3.televisores;
+
+public class Control {
+	TV tv;
+
+	public void enlazar(TV tv) {
+		this.tv = tv;
+	}
+	
+	public void setCanal(int canal) {
+		if (canal >= 1 && canal <=120 && tv.estado == true) {
+			tv.canal++;
+		}
+	}
+	public void setVolumen(int volumen) {
+		if (volumen >= 0 && volumen <=7 && tv.estado == true) {
+			tv.volumen = volumen;
+		}
+	}
+	
+	public void turnOn() {
+		tv.estado = true;
+	}
+	
+	public void turnOff() {
+		tv.estado = false;
+	}
+	
+	public void canalUp() {
+		if (tv.canal >= 1 && tv.canal <120 && tv.estado == true) {
+			tv.canal++;
+		}
+	}
+	
+	public void canalDown() {
+		if (tv.canal > 1 && tv.canal <=120 && tv.estado == true) {
+			tv.canal--;
+		}
+	}
+	
+	public void volumenUp() {
+		if (tv.volumen >= 1 && tv.volumen <7 && tv.estado == true) {
+			tv.volumen++;
+		}
+	}
+	
+	public void volumenDown() {
+		if (tv.volumen > 1 && tv.volumen <=7 && tv.estado == true) {
+			tv.volumen--;
+		}
+	}
+	
+	
+	
+	public TV getTv() {
+		return tv;
+	}
+
+	public void setTv(TV tv) {
+		this.tv = tv;
+	}
+	
+	
+}
---
+++ src/TV.java
@@ -0,0 +1,108 @@
+package taller3.televisores;
+
+public class TV {
+	Marca marca;
+	int canal = 1;
+	int precio = 500;
+	boolean estado;
+	int volumen = 1;
+	Control control;
+	public static int NumTV = 0;
+	
+	public TV(Marca marca, boolean estado) {
+		this.marca = marca;
+		this.estado = estado;
+		TV.NumTV ++;
+	}
+	
+	public void turnOn() {
+		this.estado = true;
+	}
+	
+	public void turnOff() {
+		this.estado = false;
+	}
+	
+	public void canalUp() {
+		if (canal >= 1 && canal <120 && estado == true) {
+			canal ++;
+		}
+	}
+	
+	public void canalDown() {
+		if (canal > 1 && canal <=120 && estado == true) {
+			canal--;
+		}
+	}
+	
+	public void volumenUp() {
+		if (volumen >= 0 && volumen <7 && estado == true) {
+			volumen++;
+		}
+	}
+	
+	public void volumenDown() {
+		if (volumen >= 0 && volumen <=7 && estado == true) {
+			volumen--;
+		}
+	}
+
+	public Marca getMarca() {
+		return marca;
+	}
+
+	public void setMarca(Marca marca) {
+		this.marca = marca;
+	}
+
+	public int getCanal() {
+		return canal;
+	}
+
+	public void setCanal(int canal) {
+		if (canal >= 1 && canal <=120 && estado == true) {
+			this.canal = canal;
+		}
+	}
+
+	public int getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+
+	public int getVolumen() {
+		return volumen;
+	}
+
+	public void setVolumen(int volumen) {
+		if (volumen >= 0 && volumen <=7 && estado == true) {
+			this.volumen = volumen;
+		}
+	}
+
+	public Control getControl() {
+		return control;
+	}
+
+	public void setControl(Control control) {
+		this.control = control;
+	}
+
+	public static int getNumTV() {
+		return NumTV;
+	}
+
+	public static void setNumTV(int numTV) {
+		NumTV = numTV;
+	}
+	
+	
+	
+
+	
+	
+	
+}
